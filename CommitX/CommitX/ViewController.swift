0355f16f4a0b0c026f382dcb1d62079c32c58548
Milan Shah <mshah1992@live.com>
2019-01-31 16:45:24 -0500

    Initial commit

---
+++ CommitX/CommitX/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CommitX
+//
+//  Created by Milan Shah on 1/29/19.
+//  Copyright © 2019 Milan Shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
